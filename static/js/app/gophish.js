79ba4fadb6c63f97125eb26ae522bdfe20f8ef1e
Jordan <jmwright798@gmail.com>
2014-02-08 15:16:36 -0600

    Improved usability/implementation of adding/editing groups and campaigns.

--- static/js/app/gophish.js
+++ static/js/app/gophish.js
@@ -1,32 +1,42 @@
 var app = angular.module('gophish', ['ngTable', 'ngResource']);
 
 app.factory('CampaignService', function($resource) {
-    return $resource('/api/campaigns/:id?api_key=' + API_KEY);
+    return $resource('/api/campaigns/:id?api_key=' + API_KEY, {
+        id: "@id"
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    });
 });
 
 app.factory('GroupService', function($resource) {
-    return $resource('/api/groups/:id?api_key=' + API_KEY);
+    return $resource('/api/groups/:id?api_key=' + API_KEY, {
+        id: "@id"
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    });
 });
 
 app.controller('CampaignCtrl', function($scope, CampaignService) {
-    CampaignService.query(function(campaigns){
+    CampaignService.query(function(campaigns) {
         $scope.campaigns = campaigns
     })
 });
 
 app.controller('GroupCtrl', function($scope, GroupService) {
-	GroupService.query(function(groups) {
-		$scope.groups = groups
-	})
+    GroupService.query(function(groups) {
+        $scope.groups = groups
+    })
 
     $scope.editGroup = function(group) {
         if (group === 'new') {
             $scope.newGroup = true;
             $scope.group = {
                 name: '',
-                targets: [{
-                    email: ''
-                }],
+                targets: [],
                 id: 0
             };
 
@@ -34,5 +44,17 @@ app.controller('GroupCtrl', function($scope, GroupService) {
             $scope.newGroup = false;
             $scope.group = group;
         }
+    };
+
+    $scope.addTarget = function() {
+        if ($scope.newTarget.email != "") {
+            $scope.group.targets.push({
+                email: $scope.newTarget.email
+            });
+            $scope.newTarget.email = ""
+        }
+    };
+    $scope.removeTarget = function(target) {
+        $scope.group.targets.splice($scope.group.targets.indexOf(target), 1);
     };
 })
