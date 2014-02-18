788a5703d2b8f8ecf869cbf5503c28cf45ab61ef
Jordan <jmwright798@gmail.com>
2014-02-17 20:46:57 -0600

    Working on front-end integration for adding/editing campaigns

--- static/js/app/gophish.js
+++ static/js/app/gophish.js
@@ -1,7 +1,9 @@
-var app = angular.module('gophish', ['ngTable', 'ngResource']);
+var app = angular.module('gophish', ['ngTable', 'ngResource', 'ui.bootstrap']);
 
 app.factory('CampaignService', function($resource) {
-    return $resource('/api/campaigns/:id?api_key=' + API_KEY, {id: "@id"}, {
+    return $resource('/api/campaigns/:id?api_key=' + API_KEY, {
+        id: "@id"
+    }, {
         update: {
             method: 'PUT'
         }
@@ -9,7 +11,9 @@ app.factory('CampaignService', function($resource) {
 });
 
 app.factory('GroupService', function($resource) {
-    return $resource('/api/groups/:id?api_key=' + API_KEY, {id: "@id"}, {
+    return $resource('/api/groups/:id?api_key=' + API_KEY, {
+        id: "@id"
+    }, {
         update: {
             method: 'PUT'
         }
@@ -33,6 +37,69 @@ app.controller('CampaignCtrl', function($scope, CampaignService, ngTableParams) 
             })
         }
     });
+
+    $scope.addGroup = function() {
+        if ($scope.group.name != "") {
+            $scope.campaign.groups.push({
+                name: $scope.group.name
+            });
+            $scope.group.name = ""
+            $scope.editGroupTableParams.reload()
+        }
+    };
+
+    $scope.removeGroup = function(group) {
+        $scope.campaign.groups.splice($scope.campaign.groups.indexOf(group), 1);
+        $scope.editGroupTableParams.reload()
+    };
+
+    $scope.newCampaign = function() {
+        $scope.campaign = {
+            name: '',
+            groups: []
+        };
+    };
+
+    $scope.editGroupTableParams = new ngTableParams({
+        page: 1, // show first page
+        count: 10, // count per page
+        sorting: {
+            name: 'asc' // initial sorting
+        }
+    }, {
+        total: 0, // length of data
+        getData: function($defer, params) {
+            params.total($scope.campaign.groups.length)
+            $defer.resolve($scope.campaign.groups.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
+
+    $scope.getGroups = function(val) {
+        return $http.get('/api/groups/lookup/', {
+            params: {
+                q: val
+            }
+        }).then(function(res) {
+            var groups = [];
+            angular.forEach(res.data.groups, function(item) {
+                addresses.push(item.group);
+            });
+            return groups;
+        });
+    };
+
+    $scope.saveCampaign = function(campaign) {
+        var newCampaign = new CampaignService(campaign);
+        newCampaign.$save({}, function() {
+            $scope.campaigns.push(newCampaign);
+            $scope.mainTableParams.reload()
+        });
+        $scope.group = {
+            name: '',
+            targets: [],
+        };
+        $scope.editGroupTableParams.reload()
+    }
 });
 
 app.controller('GroupCtrl', function($scope, GroupService, ngTableParams) {
@@ -98,22 +165,26 @@ app.controller('GroupCtrl', function($scope, GroupService, ngTableParams) {
     $scope.saveGroup = function(group) {
         var newGroup = new GroupService(group);
         if ($scope.newGroup) {
-            newGroup.$save({},function() {
+            newGroup.$save({}, function() {
                 $scope.groups.push(newGroup);
                 $scope.mainTableParams.reload()
             });
         } else {
-            newGroup.$update({id : newGroup.id})
+            newGroup.$update({
+                id: newGroup.id
+            })
         }
         $scope.group = {
-                name: '',
-                targets: [],
-            };
+            name: '',
+            targets: [],
+        };
         $scope.editGroupTableParams.reload()
     }
     $scope.deleteGroup = function(group) {
         var deleteGroup = new GroupService(group);
-        deleteGroup.$delete({id : deleteGroup.id}, function() {
+        deleteGroup.$delete({
+            id: deleteGroup.id
+        }, function() {
             $scope.mainTableParams.reload();
         });
     }
