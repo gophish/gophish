490ba3177967829f252b8ada3ad386e29a800254
Jordan <jmwright798@gmail.com>
2014-03-20 11:58:24 -0500

    Template HTML Preview is now editable

--- static/js/app/gophish.js
+++ static/js/app/gophish.js
@@ -1,11 +1,5 @@
 var app = angular.module('gophish', ['ngTable', 'ngResource', 'ui.bootstrap']);
 
-app.filter('unsafe', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
-});
-
 app.factory('CampaignService', function($resource) {
     return $resource('/api/campaigns/:id?api_key=' + API_KEY, {
         id: "@id"
@@ -297,3 +291,36 @@ app.controller('TemplateCtrl', function($scope, TemplateService, ngTableParams) 
         });
     }
 })
+
+// Example provided by http://docs.angularjs.org/api/ng/type/ngModel.NgModelController
+app.directive('contenteditable', function() {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+        if(!ngModel) return; // do nothing if no ng-model
+ 
+        // Specify how UI should be updated
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || '');
+        };
+ 
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function() {
+          scope.$apply(read);
+        });
+        read(); // initialize
+ 
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable the browser leaves a <br> behind
+          // If strip-br attribute is provided then we strip this out
+          if( attrs.stripBr && html == '<br>' ) {
+            html = '';
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  });
