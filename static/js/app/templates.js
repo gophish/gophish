62fc7b2d97245e96c7350f49d9db8af3a1d3a844
unknown <jmwright798@gmail.com>
2015-06-25 02:53:03 -0500

    Made progress handling adding attachments to templates

--- static/js/app/templates.js
+++ static/js/app/templates.js
@@ -1,23 +1,26 @@
-// Save attempts to POST to /groups/
+var template = {attachments:[]}
+var icons = {
+    "application/vnd.ms-excel" : "fa-file-excel-o",
+    "text/plain" : "fa-file-text-o",
+    "image/gif" : "fa-file-image-o",
+    "image/png" : "fa-file-image-o",
+    "application/pdf" : "fa-file-pdf-o",
+    "application/x-zip-compressed" : "fa-file-archive-o",
+    "application/x-gzip" : "fa-file-archive-o",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" : "fa-file-powerpoint-o",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" : "fa-file-word-o",
+    "application/octet-stream" : "fa-file-o",
+    "application/x-msdownload" : "fa-file-o"
+}
+
+// Save attempts to POST to /templates/
 function save(){
-    var targets = []
-    $.each($("#targetsTable").DataTable().rows().data(), function(i, target){
-        targets.push({
-            first_name : target[0],
-            last_name: target[1],
-            email: target[2],
-            position: target[3]
-        })
-    })
-    var group = {
-        name: $("#name").val(),
-        targets: targets
-    }
-    console.log(group)
-    // Submit the group
-    api.groups.post(group)
+    template.name = $("#name").val()
+    console.log(template)
+    // Submit the template
+    api.templates.post(template)
     .success(function(data){
-        successFlash("Group added successfully!")
+        successFlash("Template added successfully!")
         load()
         dismiss()
     })
@@ -30,36 +33,36 @@ function save(){
 function dismiss(){
     $("#modal\\.flashes").empty()
     $("#modal").modal('hide')
+    template = {attachments:[]}
 }
 
-function edit(group){
+function attach(files){
+    $.each(files, function(i, file){
+        var reader = new FileReader();
+        /* Make this a datatable */
+        reader.onload = function(e){
+            // Add the attachment
+            template.attachments.push({
+                name: file.name,
+                content: reader.result.split(",")[1],
+                type: file.type || "application/octet-stream"
+            })
+            var icon = icons[file.type] || "fa-file-o"
+            // Add the record to the modal
+            $("#attachments").append('<i class="fa ' + icon + '"></i>&nbsp;' + file.name + '<span class="remove-row"><i class="fa fa-trash-o"></i></span><br/>');
+        }
+        reader.onerror = function(e) {
+            console.log(e)
+        }
+        reader.readAsDataURL(file)
+    })
+}
+
+function edit(t){
     $("#html_editor").ckeditor()
-    if (group == "new") {
-        group = {}
+    if (t == "new") {
+        template = {attachments:[]}
     }
-    targets = $("#targetsTable").dataTable()
-    // Handle Addition
-    $("#targetForm").submit(function(){
-        targets.DataTable()
-        .row.add([
-            $("#firstName").val(),
-            $("#lastName").val(),
-            $("#email").val(),
-            $("#position").val(),
-            '<span style="cursor:pointer;"><i class="fa fa-trash-o"></i></span>'
-        ])
-        .draw()
-        $("#targetForm>div>input").val('')
-        $("#firstName").focus()
-        return false
-    })
-    // Handle Deletion
-    $("#targetsTable").on("click", "span>i.fa-trash-o", function(){
-        targets.DataTable()
-        .row( $(this).parents('tr') )
-        .remove()
-        .draw();
-    })
 }
 
 function load(){
