1183cc1bee949a24aaf067b2f2569acff7f2df6b
unknown <jmwright798@gmail.com>
2015-07-27 19:44:10 -0500

    Removed chartjs in favor of chartist-js (better interaction, more customization, etc.) Made a poc of the dashboard charts - need to work on styling. Some basic css / nav changes

--- static/js/app/dashboard.js
+++ static/js/app/dashboard.js
@@ -1,23 +1,30 @@
-var overview_chart_options = {
-    animationEasing:"linear",
-    customTooltips: function(tooltip) {
-        console.log(tooltip)
-    }
-}
+var campaigns = []
 
-function load(){
+$(document).ready(function(){
     api.campaigns.get()
-    .success(function(campaigns){
+    .success(function(cs){
+        campaigns = cs
         if (campaigns.length > 0){
-            var overview_ctx = $("#overview_chart").get(0).getContext("2d");
             // Create the overview chart data
-            var overview_data = {labels:[],datasets:[{data:[]}]}
+            var overview_data = {labels:[],series:[[]]}
+            var average_data = {series:[]}
+            var overview_opts = {
+                axisX: {
+                    showGrid: false
+                },
+                showArea: true,
+                plugins: []
+            }
+            var average_opts = {
+                donut : true,
+                donutWidth: 30
+            }
             var average = 0
             $("#emptyMessage").hide()
             $("#campaignTable").show()
             campaignTable = $("#campaignTable").DataTable();
             $.each(campaigns, function(i, campaign){
-                var campaign_date = moment(campaign.created_date).format('MMMM Do YYYY, h:mm:ss a')
+                var campaign_date = moment(campaign.created_date).format('MMMM Do YYYY h:mm')
                 // Add it to the table
                 campaignTable.row.add([
                     campaign.name,
@@ -25,7 +32,6 @@ function load(){
                     campaign.status
                 ]).draw()
                 // Add it to the chart data
-                overview_data.labels.push(campaign_date)
                 campaign.y = 0
                 $.each(campaign.results, function(j, result){
                     if (result.status == "Success"){
@@ -34,17 +40,45 @@ function load(){
                 })
                 campaign.y = Math.floor((campaign.y / campaign.results.length) * 100)
                 average += campaign.y
-                overview_data.datasets[0].data.push({y:campaign.y, test: "test"})
+                // Add the data to the overview chart
+                overview_data.labels.push(campaign_date)
+                overview_data.series[0].push({meta : i, value: campaign.y})
             })
             average = Math.floor(average / campaigns.length);
-            var overview_chart = new Chart(overview_ctx).Line(overview_data, overview_chart_options);
+            average_data.series.push({meta: "Successful Phishes", value: average})
+            average_data.series.push({meta: "Unsuccessful Phishes", value: 100 - average})
+            var average_chart = new Chartist.Pie("#average_chart", average_data, average_opts)
+            var overview_chart = new Chartist.Line('#overview_chart', overview_data, overview_opts)
+            $chart = $("#overview_chart")
+            $chart.on("click", '.ct-point', function(d){console.log(d)});
+            var $toolTip = $chart
+            .append('<div class="chartist-tooltip"></div>')
+            .find('.chartist-tooltip')
+            .hide();
+
+            $chart.on('mouseenter', '.ct-point', function() {
+                var $point = $(this)
+                value = $point.attr('ct:value')
+                cidx = $point.attr('ct:meta')
+                $toolTip.html(campaigns[cidx].name + '<br>' + "Successes: " + value.toString()).show();
+            });
+
+            $chart.on('mouseleave', '.ct-point', function() {
+                $toolTip.hide();
+            });
+            $chart.on('mousemove', function(event) {
+                $toolTip.css({
+                    left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
+                    top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height() - 40
+                });
+            });
+            $("#overview_chart").on("click", ".ct-point", function(e) {
+                var $cidx = $(this).attr('ct:meta');
+                window.location.href = "/campaigns/" + campaigns[cidx].id
+            });
         }
     })
     .error(function(){
         errorFlash("Error fetching campaigns")
     })
-}
-
-$(document).ready(function(){
-    load()
 })
