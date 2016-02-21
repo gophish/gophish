b7396fa14924bdb583a155747b8b27606829f38f
William Woodson <wjwoodson@gmail.com>
2016-02-20 20:52:38 -0600

    Added DB columns to SMTP table

--- db/migrations/20160221001538_0.1.2_store-smtp-settings.sql
+++ db/migrations/20160221001538_0.1.2_store-smtp-settings.sql
@@ -5,8 +5,8 @@ ALTER TABLE campaigns ADD COLUMN smtp_id bigint;
 UPDATE campaigns SET smtp_id = (SELECT smtp.smtp_id FROM smtp) WHERE campaigns.id = (SELECT smtp.smtp_id FROM smtp,campaigns WHERE smtp.campaign_id=campaigns.id);  -- sure hope the current smtp table works like I think it does
 
 ALTER TABLE smtp RENAME TO smtp_old;
-CREATE TABLE smtp(id integer primary key autoincrement,host varchar(255),username varchar(255),from_address varchar(255) ,ignore_cert_errors BOOLEAN);
-INSERT INTO smtp SELECT smtp_id AS id,host,username,from_address,ignore_cert_errors FROM smtp_old;
+CREATE TABLE smtp(id integer primary key autoincrement,host varchar(255),username varchar(255),from_address varchar(255),modified_date datetime default CURRENT_TIMESTAMP,ignore_cert_errors BOOLEAN);
+INSERT INTO smtp SELECT smtp_id AS id,host,username,from_address,'',ignore_cert_errors FROM smtp_old;
 DROP TABLE smtp_old;
 
 -- +goose Down
