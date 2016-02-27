f1d1d6838bb3ee13650a39baeed89f7c9ff29686
William Woodson <wjwoodson@gmail.com>
2016-02-27 08:34:33 -0600

    Updated js on sending_profiles page to work with updated /util/send_test_email

--- static/js/app/sending_profiles.js
+++ static/js/app/sending_profiles.js
@@ -3,22 +3,13 @@ var profiles = []
 // Attempts to send a test email by POSTing to /campaigns/
 function sendTestEmail() {
     var test_email_request = {
-	template: { 
-	    name: '_send_test_email_',
-	    subject: 'test email from gophish',
-	    html: 'test email from gophish'
-	},
+	template: {},
 	first_name: $("input[name=to_first_name]").val(),
 	last_name: $("input[name=to_last_name]").val(),
 	email: $("input[name=to_email]").val(),
 	position: $("input[name=to_position]").val(),
 	url: '',
-	page: { 
-	    name: '_send_test_email_',
-	    html: 'test email from gophish'
-	},
 	smtp: {
-	    name: '_send_test_email_',
 	    from_address: $("#from").val(),
 	    host: $("#host").val(),
 	    username: $("#username").val(),
