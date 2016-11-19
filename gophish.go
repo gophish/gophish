b3da41c7b89b31dd8bde1871ceef843c6d7b70ab
Jordan Wright <jmwright798@gmail.com>
2016-11-19 11:45:57 -0600

    Moved CheckAndCreateSSL to util package

--- gophish.go
+++ gophish.go
@@ -26,26 +26,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"errors"
 	"fmt"
 	"log"
-	"math/big"
 	"net/http"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/NYTimes/gziphandler"
 	"github.com/gophish/gophish/auth"
 	"github.com/gophish/gophish/config"
 	"github.com/gophish/gophish/controllers"
 	"github.com/gophish/gophish/models"
+	"github.com/gophish/gophish/util"
 	"github.com/gorilla/handlers"
 )
 
@@ -65,11 +57,11 @@ func main() {
 		adminHandler := gziphandler.GzipHandler(controllers.CreateAdminRouter())
 		auth.Store.Options.Secure = config.Conf.AdminConf.UseTLS
 		if config.Conf.AdminConf.UseTLS { // use TLS for Admin web server if available
-			Logger.Printf("Starting admin server at https://%s\n", config.Conf.AdminConf.ListenURL)
-			err := CheckAndCreateSSL(config.Conf.AdminConf.ListenURL, config.Conf.AdminConf.CertPath, config.Conf.AdminConf.KeyPath, Logger)
+			err := util.CheckAndCreateSSL(config.Conf.AdminConf.CertPath, config.Conf.AdminConf.KeyPath)
 			if err != nil {
 				Logger.Fatal(err)
 			}
+			Logger.Printf("Starting admin server at https://%s\n", config.Conf.AdminConf.ListenURL)
 			Logger.Fatal(http.ListenAndServeTLS(config.Conf.AdminConf.ListenURL, config.Conf.AdminConf.CertPath, config.Conf.AdminConf.KeyPath,
 				handlers.CombinedLoggingHandler(os.Stdout, adminHandler)))
 		} else {
@@ -91,71 +83,4 @@ func main() {
 		}
 	}()
 	wg.Wait()
-}
-
-func CheckAndCreateSSL(ListenURL string, CertPath string, KeyPath string, Logger *log.Logger) error {
-	// Check whether there is an existing SSL certificate and/or key, and if so, abort execution of this function
-	if _, err := os.Stat(CertPath); !os.IsNotExist(err) {
-		return nil
-	}
-	if _, err := os.Stat(KeyPath); !os.IsNotExist(err) {
-		return nil
-	}
-
-	Logger.Printf("Creating new self-signed certificates for administration interface...\n")
-
-	// RSA would be a perfectly fine alternative
-	priv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
-
-	notBefore := time.Now()
-	// Generate a certificate that lasts for 10 years
-	notAfter := notBefore.Add(10 * 365 * 24 * time.Hour)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-
-	if err != nil {
-		return errors.New(fmt.Sprintf("TLS Certificate Generation: Failed to generate a random serial number: %s", err))
-	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"Acme Co"},
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.Public(), priv)
-	if err != nil {
-		return errors.New(fmt.Sprintf("TLS Certificate Generation: Failed to create certificate: %s", err))
-	}
-
-	certOut, err := os.Create(CertPath)
-	if err != nil {
-		return errors.New(fmt.Sprintf("TLS Certificate Generation: Failed to open %s for writing: %s", CertPath, err))
-	}
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-
-	keyOut, err := os.OpenFile(KeyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return errors.New(fmt.Sprintf("TLS Certificate Generation: Failed to open %s for writing", KeyPath))
-	}
-
-	b, err := x509.MarshalECPrivateKey(priv)
-	if err != nil {
-		return errors.New(fmt.Sprintf("TLS Certificate Generation: Unable to marshal ECDSA private key: %v", err))
-	}
-
-	pem.Encode(keyOut, &pem.Block{Type: "EC PRIVATE KEY", Bytes: b})
-	keyOut.Close()
-
-	Logger.Println("TLS Certificate Generation complete")
-	return nil
 }
