ab1ac797a9f8557eb08edc1390b267e02640c2e6
s vignesh <vigneshs.kannan@gmail.com>
2016-12-03 05:08:20 +0530

    Attachment Bug Fix (#471)
    Fixes issue with cropped attachments.

---
+++ db/db_mysql/migrations/20161202153627_AttachmentFix.sql
@@ -0,0 +1,9 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE attachments MODIFY content LONGTEXT;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+ALTER TABLE attachments MODIFY content TEXT;
