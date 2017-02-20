42f2748096528943a031c4c335e6776095c5ca4c
Jordan Wright <jmwright798@gmail.com>
2017-02-19 18:22:26 -0600

    Attempting to fix mysql migration

--- db/db_mysql/migrations/20170219122503_0.2.1_email_headers.sql
+++ db/db_mysql/migrations/20170219122503_0.2.1_email_headers.sql
@@ -2,7 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE IF NOT EXISTS headers(
-	id integer primary key autoincrement,
+	id integer primary key auto_increment,
 	key varchar(255),
 	value varchar(255),
 	"smtp_id" bigint
