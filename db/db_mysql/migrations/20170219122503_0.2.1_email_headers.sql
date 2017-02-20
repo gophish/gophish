aac6b58a704ee263865db4db49de9c371a228cb2
Jordan Wright <jmwright798@gmail.com>
2017-02-19 18:28:04 -0600

    Fixing MySQL migration to quote reserved words

--- db/db_mysql/migrations/20170219122503_0.2.1_email_headers.sql
+++ db/db_mysql/migrations/20170219122503_0.2.1_email_headers.sql
@@ -3,9 +3,9 @@
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE IF NOT EXISTS headers(
 	id integer primary key auto_increment,
-	key varchar(255),
-	value varchar(255),
-	"smtp_id" bigint
+	`key` varchar(255),
+	`value` varchar(255),
+	`smtp_id` bigint
 );
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
