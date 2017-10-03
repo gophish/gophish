3972fe5993ec409bd94bd9f3ca52b534d0f9a8f4
Jordan Wright <jmwright798@gmail.com>
2017-10-02 19:40:56 -0500

    Fixing dev dependencies
    Fixes #775
    Fixes #776

--- gulpfile.js
+++ gulpfile.js
@@ -20,26 +20,26 @@ var gulp = require('gulp'),
 gulp.task('vendorjs', function () {
     // Vendor minifying / concat
     return gulp.src([
-        vendor_directory + 'jquery.js',
-        vendor_directory + 'bootstrap.min.js',
-        vendor_directory + 'moment.min.js',
-        vendor_directory + 'chartist.min.js',
-        vendor_directory + 'papaparse.min.js',
-        vendor_directory + 'd3.min.js',
-        vendor_directory + 'topojson.min.js',
-        vendor_directory + 'datamaps.min.js',
-        vendor_directory + 'jquery.dataTables.min.js',
-        vendor_directory + 'dataTables.bootstrap.js',
-        vendor_directory + 'datetime-moment.js',
-        vendor_directory + 'jquery.ui.widget.js',
-        vendor_directory + 'jquery.fileupload.js',
-        vendor_directory + 'jquery.iframe-transport.js',
-        vendor_directory + 'sweetalert2.min.js',
-        vendor_directory + 'bootstrap-datetime.js',
-        vendor_directory + 'select2.min.js',
-        vendor_directory + 'core.min.js',
-        vendor_directory + 'highcharts.js'
-    ])
+            vendor_directory + 'jquery.js',
+            vendor_directory + 'bootstrap.min.js',
+            vendor_directory + 'moment.min.js',
+            vendor_directory + 'chartist.min.js',
+            vendor_directory + 'papaparse.min.js',
+            vendor_directory + 'd3.min.js',
+            vendor_directory + 'topojson.min.js',
+            vendor_directory + 'datamaps.min.js',
+            vendor_directory + 'jquery.dataTables.min.js',
+            vendor_directory + 'dataTables.bootstrap.js',
+            vendor_directory + 'datetime-moment.js',
+            vendor_directory + 'jquery.ui.widget.js',
+            vendor_directory + 'jquery.fileupload.js',
+            vendor_directory + 'jquery.iframe-transport.js',
+            vendor_directory + 'sweetalert2.min.js',
+            vendor_directory + 'bootstrap-datetime.js',
+            vendor_directory + 'select2.min.js',
+            vendor_directory + 'core.min.js',
+            vendor_directory + 'highcharts.js'
+        ])
         .pipe(concat('vendor.js'))
         .pipe(rename({
             suffix: '.min'
@@ -60,22 +60,24 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest(dest_js_directory + 'app/'));
 })
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src([
-        css_directory + 'bootstrap.min.css',
-        css_directory + 'main.css',
-        css_directory + 'dashboard.css',
-        css_directory + 'flat-ui.css',
-        css_directory + 'dataTables.bootstrap.css',
-        css_directory + 'font-awesome.min.css',
-        css_directory + 'chartist.min.css',
-        css_directory + 'bootstrap-datetime.css',
-        css_directory + 'checkbox.css',
-        css_directory + 'sweetalert2.min.css',
-        css_directory + 'select2.min.css',
-        css_directory + 'select2-bootstrap.min.css',
-    ])
-        .pipe(cleanCSS({ compatibilty: 'ie9' }))
+            css_directory + 'bootstrap.min.css',
+            css_directory + 'main.css',
+            css_directory + 'dashboard.css',
+            css_directory + 'flat-ui.css',
+            css_directory + 'dataTables.bootstrap.css',
+            css_directory + 'font-awesome.min.css',
+            css_directory + 'chartist.min.css',
+            css_directory + 'bootstrap-datetime.css',
+            css_directory + 'checkbox.css',
+            css_directory + 'sweetalert2.min.css',
+            css_directory + 'select2.min.css',
+            css_directory + 'select2-bootstrap.min.css',
+        ])
+        .pipe(cleanCSS({
+            compatibilty: 'ie9'
+        }))
         .pipe(concat('gophish.css'))
         .pipe(gulp.dest(dest_css_directory));
 })
