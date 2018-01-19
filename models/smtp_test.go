23eb37a42ce188fd998b09e81ffa7a4294f6291b
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-19 19:07:28 +0900

    move SMTP related tests from models_test.go to smtp_test.go

--- models/smtp_test.go
+++ models/smtp_test.go
@@ -6,6 +6,58 @@ import (
 	check "gopkg.in/check.v1"
 )
 
+func (s *ModelsSuite) TestPostSMTP(c *check.C) {
+	smtp := SMTP{
+		Name:        "Test SMTP",
+		Host:        "1.1.1.1:25",
+		FromAddress: "Foo Bar <foo@example.com>",
+		UserId:      1,
+	}
+	err = PostSMTP(&smtp)
+	c.Assert(err, check.Equals, nil)
+	ss, err := GetSMTPs(1)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(len(ss), check.Equals, 1)
+}
+
+func (s *ModelsSuite) TestPostSMTPNoHost(c *check.C) {
+	smtp := SMTP{
+		Name:        "Test SMTP",
+		FromAddress: "Foo Bar <foo@example.com>",
+		UserId:      1,
+	}
+	err = PostSMTP(&smtp)
+	c.Assert(err, check.Equals, ErrHostNotSpecified)
+}
+
+func (s *ModelsSuite) TestPostSMTPNoFrom(c *check.C) {
+	smtp := SMTP{
+		Name:   "Test SMTP",
+		UserId: 1,
+		Host:   "1.1.1.1:25",
+	}
+	err = PostSMTP(&smtp)
+	c.Assert(err, check.Equals, ErrFromAddressNotSpecified)
+}
+
+func (s *ModelsSuite) TestPostSMTPValidHeader(c *check.C) {
+	smtp := SMTP{
+		Name:        "Test SMTP",
+		Host:        "1.1.1.1:25",
+		FromAddress: "Foo Bar <foo@example.com>",
+		UserId:      1,
+		Headers: []Header{
+			Header{Key: "Reply-To", Value: "test@example.com"},
+			Header{Key: "X-Mailer", Value: "gophish"},
+		},
+	}
+	err = PostSMTP(&smtp)
+	c.Assert(err, check.Equals, nil)
+	ss, err := GetSMTPs(1)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(len(ss), check.Equals, 1)
+}
+
 func (s *ModelsSuite) TestSMTPGetDialer(ch *check.C) {
 	host := "localhost"
 	port := 25
