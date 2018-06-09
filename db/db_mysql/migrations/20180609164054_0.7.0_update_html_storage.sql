da6091e0212782f8784fb54addca343484fda301
Jordan Wright <jmwright798@gmail.com>
2018-06-09 18:10:27 -0500

    Add migration to change MySQL storage of HTML to be mediumtext to hold larger HTML pages. Fixes #1067

---
+++ db/db_mysql/migrations/20180609164054_0.7.0_update_html_storage.sql
@@ -0,0 +1,10 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE templates MODIFY html MEDIUMTEXT;
+ALTER TABLE pages MODIFY html MEDIUMTEXT;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+ALTER TABLE templates MODIFY html TEXT;
+ALTER TABLE pages MODIFY html TEXT;
