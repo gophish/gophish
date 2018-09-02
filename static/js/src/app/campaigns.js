4146e14fec05c467499dd72893cb820eca4b5150
Jordan Wright <jmwright798@gmail.com>
2018-09-02 10:50:46 -0500

    Fixed default send_by_date parsing on the frontend

--- static/js/src/app/campaigns.js
+++ static/js/src/app/campaigns.js
@@ -33,6 +33,10 @@ function launch() {
                     });
                 })
                 // Validate our fields
+                var send_by_date = $("#send_by_date").val()
+                if (send_by_date != "") {
+                    send_by_date = moment(send_by_date, "MM/DD/YYYY hh:mm a").utc().format()
+                }
                 campaign = {
                     name: $("#name").val(),
                     template: {
@@ -46,7 +50,7 @@ function launch() {
                         name: $("#profile").select2("data")[0].text
                     },
                     launch_date: moment($("#launch_date").val(), "MM/DD/YYYY hh:mm a").utc().format(),
-                    send_by_date: moment($("#send_by_date").val(), "MM/DD/YYYY hh:mm a").utc().format(),
+                    send_by_date: send_by_date || null,
                     groups: groups,
                 }
                 console.log("Launching campaign at time: " + campaign.launch_date)
@@ -272,7 +276,8 @@ $(document).ready(function () {
         "widgetPositioning": {
             "vertical": "bottom"
         },
-        "showTodayButton": true
+        "showTodayButton": true,
+        "useCurrent": false
     })
     // Setup multiple modals
     // Code based on http://miles-by-motorcycle.com/static/bootstrap-modal/index.html
