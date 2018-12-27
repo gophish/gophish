191ec6e43631eb80ba763358e447e2cef9b9e24f
Jordan Wright <jmwright798@gmail.com>
2018-12-27 15:04:24 -0600

    Added the CKEditor link dialog fixes to the email templates

--- static/js/src/app/templates.js
+++ static/js/src/app/templates.js
@@ -388,6 +388,21 @@ $(document).ready(function () {
     $("#importEmailModal").on('hidden.bs.modal', function (event) {
         $("#email_content").val("")
     })
+    CKEDITOR.on('dialogDefinition', function (ev) {
+        // Take the dialog name and its definition from the event data.
+        var dialogName = ev.data.name;
+        var dialogDefinition = ev.data.definition;
+
+        // Check if the definition is from the dialog window you are interested in (the "Link" dialog window).
+        if (dialogName == 'link') {
+            dialogDefinition.minWidth = 500
+            dialogDefinition.minHeight = 100
+
+            // Remove the linkType field
+            var infoTab = dialogDefinition.getContents('info');
+            infoTab.get('linkType').hidden = true;
+        }
+    });
     load()
 
 })
