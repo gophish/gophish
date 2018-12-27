ba967a724413245dc58df843568a77e3cd8bd956
Jordan Wright <jmwright798@gmail.com>
2018-12-27 10:23:54 -0600

    Added quotes for table names in MySQL migrations to avoid clashing with reserved keywords. Fixes #1325 (#1329)

--- db/db_mysql/migrations/20171208201932_0.4.1_next_send_date.sql
+++ db/db_mysql/migrations/20171208201932_0.4.1_next_send_date.sql
@@ -1,7 +1,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE results ADD COLUMN send_date DATETIME;
+ALTER TABLE `results` ADD COLUMN send_date DATETIME;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
