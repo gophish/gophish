619bc4c18d43f378a25b7cb554f5a92287e425f9
alex maslakoff <alex@serendipia.email>
2019-10-31 16:32:39 +0700

    wip

---
+++ static/js/src/app/webhooks.js
@@ -0,0 +1,21 @@
+let webhooks = []
+
+
+//TODO
+const load = () => {
+    $("#loading").show()
+    api.webhooks.get()
+        .success((us) => {
+            $("#loading").hide()
+        })
+        .error(() => {
+            errorFlash("Error fetching webhooks")
+        })
+}
+
+$(document).ready(function () {
+    load()
+
+
+
+});
---
+++ webhook/webhook.go
@@ -0,0 +1,38 @@
+package webhook
+
+type Webhook struct {
+  client *http.Client
+}
+
+func (whook *Webhook) Send(server string, secret []byte, data interface{}) error {
+  resp, err := http.Post(server, ???)
+}
+
+
+//TODO
+func (as *Server) Webhook(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+
+  switch {
+  case r.Method == "GET":
+    JSONResponse(w, c, http.StatusOK)
+
+  case r.Method == "POST":
+    JSONResponse(w, c, http.StatusOK)
+
+  case r.Method == "DELETE":
+    JSONResponse(w, c, http.StatusOK)
+
+
+
+  }
+}
+
+func (as *Server) Ping(w http.ResponseWriter, r *http.Request) {
+  switch {
+  case r.Method == "POST":
+    //TODO
+
+
+  }
+}
