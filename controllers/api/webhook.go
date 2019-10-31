40d11267843b20608c39aa693a48b58d58e1bdce
alex maslakoff <alex@serendipia.email>
2019-10-31 21:21:14 +0700

    wip

--- controllers/api/webhook.go
+++ controllers/api/webhook.go
@@ -13,7 +13,56 @@ import (
 
 
 func (as *Server) Webhooks(w http.ResponseWriter, r *http.Request) {
-
+  switch {
+  case r.Method == "GET":
+    us, err := models.GetWebhooks()
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusInternalServerError)
+      return
+    }
+    JSONResponse(w, us, http.StatusOK)
+    return
+  case r.Method == "POST":
+    ur := &userRequest{}
+    err := json.NewDecoder(r.Body).Decode(ur)
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusBadRequest)
+      return
+    }
+    err = ur.Validate(nil)
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusBadRequest)
+      return
+    }
+    if ur.Password == "" {
+      JSONResponse(w, models.Response{Success: false, Message: ErrEmptyPassword.Error()}, http.StatusBadRequest)
+      return
+    }
+    hash, err := util.NewHash(ur.Password)
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusInternalServerError)
+      return
+    }
+    role, err := models.GetRoleBySlug(ur.Role)
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusInternalServerError)
+      return
+    }
+    user := models.User{
+      Username: ur.Username,
+      Hash:     hash,
+      ApiKey:   util.GenerateSecureKey(),
+      Role:     role,
+      RoleID:   role.ID,
+    }
+    err = models.PutUser(&user)
+    if err != nil {
+      JSONResponse(w, models.Response{Success: false, Message: err.Error()}, http.StatusInternalServerError)
+      return
+    }
+    JSONResponse(w, user, http.StatusOK)
+    return
+  }
   JSONResponse(w, "TODO", http.StatusOK)
   return
 }
--- webhook/webhook.go
+++ webhook/webhook.go
@@ -5,7 +5,38 @@ type Webhook struct {
 }
 
 func (whook *Webhook) Send(server string, secret []byte, data interface{}) error {
-  resp, err := http.Post(server, ???)
+  jsonData, err := json.Marshal(data)
+  if err != nil {
+    http.Error(w, "Error converting data parameter to JSON", http.StatusInternalServerError)
+    log.Error(err)
+  }
+
+
+              w.Header().Set("Content-Type", "application/json")
+              w.WriteHeader(c)
+              fmt.Fprintf(w, "%s", dj)
+
+
+
+
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+
+  //TODO
+  sign := ""
+  req.Header.Set("X-Gophish-Signature", sign)
+  req.Header.Set("Content-Type", "application/json")
+
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    panic(err)
+  }
+  defer resp.Body.Close()
+
+  if resp.Status >= 400 {
+    //error
+  }
+
 }
 
 
