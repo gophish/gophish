ff58e6e4e7c30fdc4cfbef2afbc999d268d31efd
alex maslakoff <alex@serendipia.email>
2019-11-04 21:42:59 +0700

    wip

--- static/js/src/app/webhooks.js
+++ static/js/src/app/webhooks.js
@@ -20,14 +20,18 @@ const load = () => {
             $.each(webhooks, (i, webhook) => {
                 webhookTable.row.add([
                     escapeHtml(webhook.title),
+                    escapeHtml(webhook.url),
                     `
-                      <div class='pull-right'>
-                      <button class='btn btn-primary edit_button' data-toggle='modal' data-backdrop='static' data-target='#modal' data-webhook-id='${webhook.id}'>
-                        <i class='fa fa-pencil'></i>
-                      </button>
-                      <button class='btn btn-danger delete_button' data-webhook-id='${webhook.id}'>
-                        <i class='fa fa-trash-o'></i>
-                      </button>
+                      <div class="pull-right">
+                        <button class="btn btn-primary validate_button" data-backdrop="static" data-webhook-id="${webhook.id}">
+                          Ping
+                        </button>
+                        <button class="btn btn-primary edit_button" data-toggle="modal" data-backdrop="static" data-target="#modal" data-webhook-id="${webhook.id}">
+                          <i class="fa fa-pencil"></i>
+                        </button>
+                        <button class="btn btn-danger delete_button" data-webhook-id="${webhook.id}">
+                          <i class="fa fa-trash-o"></i>
+                        </button>
                       </div>
                     `
                 ]).draw()
