f79d7238e582a32d3224102cbd0badf8cc9e6de8
alex maslakoff <alex@serendipia.email>
2019-11-29 13:37:34 +0800

    wip

--- webhook/webhook_test.go
+++ webhook/webhook_test.go
@@ -1,1 +1,32 @@
 package webhook
+
+
+//TODO
+import (
+
+	// "bytes"
+	// "context"
+	// "errors"
+	// "io"
+	// "net/textproto"
+	// "reflect"
+	// "testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type WebhookSuite struct {
+	suite.Suite
+}
+
+// mockSender is a mock gomail.Sender used for testing.
+type mockSender struct {
+
+}
+
+func newMockSender() *mockSender {
+	ms := &mockSender{
+		
+	}
+	return ms
+}
