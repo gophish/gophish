717d5701de6c9af8bc72f217ec587b3f9763c322
Chris Zietlow <aczietlow@gmail.com>
2019-12-18 10:07:58 -0500

    Fix webhooks db migration.

--- db/db_mysql/migrations/20191104103306_0.9.0_create_webhooks.sql
+++ db/db_mysql/migrations/20191104103306_0.9.0_create_webhooks.sql
@@ -1,12 +1,12 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE IF NOT EXISTS "webhooks" (
-    "id" integer primary key autoincrement,
-    "name" varchar(255),
-    "url" varchar(1000),
-    "secret" varchar(255),
-    "is_active" boolean default 0
+CREATE TABLE IF NOT EXISTS `webhooks` (
+    id integer primary key auto_increment,
+    name varchar(255),
+    url varchar(1000),
+    secret varchar(255),
+    is_active boolean default 0
 );
 
 
