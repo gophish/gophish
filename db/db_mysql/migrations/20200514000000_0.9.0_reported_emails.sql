935d0fc47256395c810b8f702269791cd99fe822
Glenn Wilkinson <glenn.wilkinson@gmail.com>
2020-07-17 18:14:37 +0100

    Fixed and tested MySQL support

--- db/db_mysql/migrations/20200514000000_0.9.0_reported_emails.sql
+++ db/db_mysql/migrations/20200514000000_0.9.0_reported_emails.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE IF NOT EXISTS `reported` (id bigint primary key autoincrement, user_id integer ,reported_by_name varchar(255), reported_by_email varchar(255), reported_time datetime, reported_html varchar(255), reported_text varchar(255), reported_subject varchar(255),imap_uid varchar(255), status varchar(255), notes varchar(255));
+CREATE TABLE IF NOT EXISTS `reported` (id bigint primary key auto_increment, user_id bigint, reported_by_name varchar(255), reported_by_email varchar(255), reported_time datetime, reported_html text, reported_text text, reported_subject text, imap_uid varchar(255), status varchar(255), notes text);
 
-CREATE TABLE IF NOT EXISTS `reported_attachments` (id bigint primary key autoincrement, rid bigint, filename varchar(255), header varchar(255), size bigint, content varchar(255));
+CREATE TABLE IF NOT EXISTS `reported_attachments` (id bigint primary key auto_increment, rid bigint, filename varchar(255), header text, size bigint, content text);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
