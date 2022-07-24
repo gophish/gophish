17e7390cf96ab9460d09829fd892c83f1c0d5973
zyazhb <zyazhb@yahoo.com>
2022-07-24 10:03:15 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,17 @@ const path = require('path');
 module.exports = {
     context: path.resolve(__dirname, 'static', 'js', 'src', 'app'),
     entry: {
+        autocomplete: './autocomplete',
+        campaign_results: './campaign_results',
+        campaigns: './campaigns',
+        dashboard: './dashboard',
+        gophish: './gophish',
+        groups: './groups',
+        landing_pages: './landing_pages',
         passwords: './passwords',
+        templates: './templates',
+        sending_profiles: './sending_profiles',
+        settings: './settings',
         users: './users',
         webhooks: './webhooks',
     },
