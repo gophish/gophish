cd9a046b82f14fb03515c8f230df8235baf454cf
Mark Cabanero <me@markcabanero.com>
2022-09-03 04:50:42 +0000

    feat: Add TrustedOrigins as an admin config construct
    Enables the user to add addresses that they expect incoming connections
    to come from. Helpful in cases where TLS termination is handled by a
    load balancer upstream, rather than the application itself.
    
    This does not support wildcard hosts.

--- config/config.go
+++ config/config.go
@@ -15,6 +15,7 @@ type AdminServer struct {
 	KeyPath              string   `json:"key_path"`
 	CSRFKey              string   `json:"csrf_key"`
 	AllowedInternalHosts []string `json:"allowed_internal_hosts"`
+	TrustedOrigins       []string `json:"trusted_origins"`
 }
 
 // PhishServer represents the Phish server configuration details
