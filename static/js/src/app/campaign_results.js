b157f15e214b4e1183302e9726bae48fc032dda0
Paul Werther <pw@evait.de>
2023-09-13 14:31:58 +0200

    change icons

--- static/js/src/app/campaign_results.js
+++ static/js/src/app/campaign_results.js
@@ -416,8 +416,8 @@ var renderDevice = function (event_details) {
         deviceBrowser + ' ' + browserVersion + '</div>'
 
     var _device_ip_adr = details.browser['address'];
-    var ipaddrString ='<div class="timeline-device-browser"><span class="fa fa-stack">' + '<i class="fa fa-info-circle'+ ' fa-stack-1x"></i></span> ' + _device_ip_adr + '</div>';
-    var UAfullString ='<div class="timeline-device-browser"><span class="fa fa-stack">' + '<i class="fa fa-code'+ ' fa-stack-1x"></i></span> ' + details.browser['user-agent'] + '</div>';
+    var ipaddrString ='<div class="timeline-device-browser"><span class="fa fa-stack">' + '<i class="fa fa-map-marker'+ ' fa-stack-1x"></i></span> ' + _device_ip_adr + '</div>';
+    var UAfullString ='<div class="timeline-device-browser"><span class="fa fa-stack">' + '<i class="fa fa-user-secret'+ ' fa-stack-1x"></i></span> ' + details.browser['user-agent'] + '</div>';
 
     detailsString += browserString
     detailsString += ipaddrString;
